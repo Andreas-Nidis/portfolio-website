8041081838b1d928c80fb6c2b048101872ae1f17
Andreas Kalketenidis <a_kalketenidis@hotmail.com>
2025-01-18 21:57:08 +0100

    Added react-router-dom, styled-components, and reorganised component imports and exports.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './components/Header';
+import Main from './components/Main';
 
 
 function App() {
