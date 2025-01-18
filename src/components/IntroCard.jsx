85f35c09a91a5b6a0ceab927a508d2caeb2d04da
Andreas Kalketenidis <a_kalketenidis@hotmail.com>
2025-01-18 23:58:47 +0100

    Fixed IntroCard letter-spacing animation and layout

--- src/components/IntroCard.jsx
+++ src/components/IntroCard.jsx
@@ -1,9 +1,12 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const IntroCard = (props) => {
     return (
         <>
             <Container>
+                <Title>
+                    <h1>Welcome!</h1>
+                </Title>
                 <Bar>
                     <Photo>
                         <img src="/images/portrait/photo.png" alt="Andreas Photo"/>
@@ -14,26 +17,58 @@ const IntroCard = (props) => {
     )
 }
 
+const letterSpacingAnimation = keyframes`
+  0%, 100% {
+    letter-spacing: 3px;
+  }
+  50% {
+    letter-spacing: 8px;
+  }
+`;
+
 const Container = styled.div`
     position: relative;
     display: block;
     margin: 0;
     padding: 0;
     top: 0;
-    height: 700px;
+    height: 400px;
     width: 100%;
     display: flex;
     align-items: center;
+
+
+    @media (max-width: 968px) {
+        flex-direction: column;
+    }
+`;
+
+const Title = styled.div`
+    margin: 100px;
+    
+    h1 {
+        text-shadow: 3px 3px 1px #9d9d9d;
+        color: #474747;
+        font-family: 'Bungee Inline', cursive;
+        font-size: 5em;
+        animation: ${letterSpacingAnimation} 2s infinite ease-in-out;
+    }
+
+    @media (max-width: 968px) {
+        flex-direction: column;
+        margin: 50px;
+    }
 `;
 
 const Bar = styled.div`
     position: relative;
-    background-color: #676767;
+    background-color: #474747;
     width: 100%;
     height: 150px;
     display: flex;
     justify-content: center;
     align-items: center;
+    box-shadow: 0 0 10px #ffcd05, 0 0 20px #ffcd05, 0 0 30px #ff0505;
 `;
 
 const Photo = styled.div`
