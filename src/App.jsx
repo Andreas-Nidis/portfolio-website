03c70b81000a3eef0179978137e512d43d687e71
Andreas Kalketenidis <a_kalketenidis@hotmail.com>
2025-01-18 21:50:09 +0100

    First commit.

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} /> 
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
